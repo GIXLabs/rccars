3c35fdfd692ab06517681476ef3a9afdb053bdff
tejabalu <tejabalu15@gmail.com>
2022-07-14 17:37:39 -0700

    initial working src code

--- src/RC_car_code/RC_car_code.ino
+++ src/RC_car_code/RC_car_code.ino
@@ -1,109 +1,90 @@
-// The bluetooth Android control app can be downloaded from: https://play.google.com/store/apps/details?id=braulio.calle.bluetoothRCcontroller.
-// The documentation for the bluetooth app can be found in https://sites.google.com/site/bluetoothrccar/.
-
 #include <SoftwareSerial.h>
-#include "Wire.h"
-#include <Servo.h>
+SoftwareSerial BTserial(3, 2); 
 
-SoftwareSerial BTserial(2, 3); // RX | TX
+#define A1 5  // Motor A pins
+#define A2 6
+#define B1 10 // Motor B pins
+#define B2 11
 
-char t;
-const int B1A = 10;
-const int B1B = 11;
-Servo servo_1;
-int ang_int = 90;
-int velocity = 255;
-
-// constants won't change. They're used here to set pin numbers:
-const int buttonPin = 8;     // the number of the pushbutton pin
-const int ledPin =  12  ;      // the number of the LED pin
-int buttonState = 0;         // variable for reading the pushbutton status
-int duration = 0;
-
+int bt_input = 0; // for incoming signal from bluetooth
 void setup() {
-  pinMode(B1A, OUTPUT);
-  pinMode(B1B, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(B1, OUTPUT);
+  pinMode(B2, OUTPUT);
+
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(B1, LOW);
+  digitalWrite(B2, LOW);
   
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-  
-  Serial.begin(9600);
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+
+  Serial.println("select direction of movement");
+  Serial.println("1.forward");
+  Serial.println("2.backward");
+  Serial.println("3.stop");
   BTserial.begin(9600);
   BTserial.print("AT+NAME");
-  BTserial.print("RC_02");
-  servo_1.attach(7);
-  servo_1.write(ang_int);
-  delay(1000);
+  BTserial.print("RC_Textbed");
 }
 
-void loop() {  
-  buttonState = digitalRead(buttonPin);
-  t = BTserial.read();
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH.
-  // long press 3s for reset:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    duration++;
-    Serial.println(duration);
-    digitalWrite(ledPin, HIGH);
-  } 
-  else if (buttonState == LOW && duration > 300){
-    // turn LED off:
-    duration = 0;
-    digitalWrite(ledPin, LOW);
-  }else{
-    duration = 0;
+void loop() {
+  if (BTserial.available()) {
+    bt_input = BTserial.read();
+    Serial.write(bt_input);
+    
+    switch (bt_input) {
+      case 'F':         // if input=F, motors turn forward
+        forward();
+        break;
+      case 'B':         // if input=B, motors turn backward
+        backward();
+        break;
+      case 'S':         // if input=S, motors stop
+        Stop();
+        break;
+      case 'L':
+        left();         // if input=L, motors turn left
+        break;
+      case 'R':
+        right();       // if input=R, motors turn right
+        break;
   }
-  
-  if (t == '0') {
-    velocity = 0;
-  }  else if (t == '1') {
-    velocity = 100;
-  } else if (t == '2') {
-    velocity = 180;
-  } else if (t == '3') {
-    velocity = 200;
-  } else if (t == '4') {
-    velocity = 255;
-  }
-  
-  if (t == 'F') {
-    analogWrite(B1A, 0);
-    analogWrite(B1B,  velocity);
-    servo_1.write(ang_int);
-  }  else if (t == 'L') {
-    analogWrite(B1A, 0);
-    analogWrite(B1B,  velocity);
-    servo_1.write(ang_int + 45);
-  }  else if (t == 'R') {
-    analogWrite(B1A, 0);
-    analogWrite(B1B, velocity);
-    servo_1.write(ang_int - 45);
-  }  else if (t == 'G') {
-    analogWrite(B1A, 0);
-    analogWrite(B1B, velocity);
-    servo_1.write(ang_int + 15);
-  }  else if (t == 'I') {
-    analogWrite(B1A, 0);
-    analogWrite(B1B, velocity);
-    servo_1.write(ang_int - 15);
-  } else if (t == 'B') {
-    analogWrite(B1A, velocity);
-    analogWrite(B1B, 0);
-    servo_1.write(ang_int);
-  }  else if (t == 'H') {
-    analogWrite(B1A, velocity);
-    analogWrite(B1B, 0);
-    servo_1.write(ang_int + 30);
-  }  else if (t == 'J') {
-    analogWrite(B1A, velocity);
-    analogWrite(B1B, 0);
-    servo_1.write(ang_int - 30);
-  }  else if (t == 'S') {
-    analogWrite(B1A, 0);
-    analogWrite(B1B, 0);
-    servo_1.write(ang_int);
-  }
+}
+}
+void forward() {          // function for forward 
+  analogWrite(A1, 210);
+  analogWrite(A2, 0);
+  analogWrite(B1, 210);
+  analogWrite(B2, 0);
+}
+
+void backward() {         // function of backward
+  Serial.println("2 pressed | Backward");
+  analogWrite(A1, 0);
+  analogWrite(A2, 210);
+  analogWrite(B1, 0);
+  analogWrite(B2, 210);
+}
+
+void left() {             // function for left   
+  analogWrite(A1, 255);
+  analogWrite(A2, 0);
+  analogWrite(B1, 150);
+  analogWrite(B2, 0);
+}
+
+void right() {            // function for right
+  analogWrite(A1, 150);
+  analogWrite(A2, 0);
+  analogWrite(B1, 255);
+  analogWrite(B2, 0);
+}
+
+void Stop() {              // function for stop
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(B1, LOW);
+  digitalWrite(B2, LOW);
 }
