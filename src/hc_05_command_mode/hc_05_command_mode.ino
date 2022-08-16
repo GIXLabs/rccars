c14cc86610a36c3a72ea365df3ebb1a3f6561945
chuan <cchang30@seattlecentral.edu>
2022-08-15 17:16:56 -0700

    updated code for the proper  RX and TX pins

--- src/hc_05_command_mode/hc_05_command_mode.ino
+++ src/hc_05_command_mode/hc_05_command_mode.ino
@@ -1,11 +1,14 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial Bluetooth(3,2);
+SoftwareSerial Bluetooth(2,3);
+
+char c=' ';
 
 void setup() {
+  Serial.begin(9600);
 //  pinMode(4,OUTPUT); // this pin will pull the HC-05 pinD4 HIGH to switch module to AT mode
 //  digitalWrite(4,HIGH);
-  Serial.begin(9600);
+  
 
   Serial.println("ready for AT commands");
   Bluetooth.begin(38400);
@@ -17,9 +20,11 @@ void setup() {
 
 void loop() {
   if (Bluetooth.available()) {
-    Serial.write(Bluetooth.read());
+    c = Bluetooth.read();
+    Serial.write(c);
   }
   if (Serial.available()) {
-    Bluetooth.write(Serial.read());
+    c = Serial.read();
+    Bluetooth.write(c);
   }
 }
