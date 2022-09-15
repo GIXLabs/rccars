46edae1b10e841bcddda8f497a4431550ecf79d7
chuan <cchang30@seattlecentral.edu>
2022-09-15 12:54:06 -0700

    name change for rccar

--- src/RC_car_code/RC_car_code.ino
+++ src/RC_car_code/RC_car_code.ino
@@ -21,9 +21,9 @@ void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
 
   Serial.println("Connect to your bluetooth device, and send commands to serial port.");
-  BTserial.begin(9600);
+  BTserial.begin(38400);
   BTserial.print("AT+NAME");
-  BTserial.print("RC_Textbed");
+  BTserial.print("RCM_01");
 }
 
 void loop() {
