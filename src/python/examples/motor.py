2e72f635985dd10130a143523ef3946baa904e58
Kevin <kevin.arne@gmail.com>
2023-08-04 11:26:22 -0700

    Updated motor pins. Added Bluetooth test script.

--- src/python/examples/motor.py
+++ src/python/examples/motor.py
@@ -3,10 +3,10 @@ import board
 import pwmio
 from digitalio import DigitalInOut, Direction
 
-motora_1b = pwmio.PWMOut(board.GP14) #DigitalInOut(board.GP14) 
-motora_1a = pwmio.PWMOut(board.GP15) #DigitalInOut(board.GP15)
-motorb_1b = DigitalInOut(board.GP10)
+motora_1a = pwmio.PWMOut(board.GP6) #DigitalInOut(board.GP15)
+motora_1b = pwmio.PWMOut(board.GP5) #DigitalInOut(board.GP14) 
 motorb_1a = DigitalInOut(board.GP11)
+motorb_1b = DigitalInOut(board.GP10)
 
 motorb_1b.direction = Direction.OUTPUT
 motorb_1a.direction = Direction.OUTPUT
@@ -23,7 +23,7 @@ def stop():
     motora_1a.duty_cycle = 0
     motora_1b.duty_cycle = 0
     motorb_1b.value = False
-    motorb_1a.value = True
+    motorb_1a.value = False
 
 print("Hello World!")
 
