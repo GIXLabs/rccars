658c6558eb37af963e2b9d40078527888a5544f7
Kevin <kevin.arne@gmail.com>
2023-08-16 17:50:20 -0700

    Added pwm to both motors.

--- src/python/examples/motor.py
+++ src/python/examples/motor.py
@@ -5,27 +5,22 @@ from digitalio import DigitalInOut, Direction
 
 motora_1a = pwmio.PWMOut(board.GP6) #DigitalInOut(board.GP15)
 motora_1b = pwmio.PWMOut(board.GP5) #DigitalInOut(board.GP14) 
-motorb_1a = DigitalInOut(board.GP11)
-motorb_1b = DigitalInOut(board.GP10)
-
-motorb_1b.direction = Direction.OUTPUT
-motorb_1a.direction = Direction.OUTPUT
+motorb_1a = pwmio.PWMOut(board.GP11) #DigitalInOut(board.GP11)
+motorb_1b = pwmio.PWMOut(board.GP10) #DigitalInOut(board.GP10)
 
 speed = 65535 # 16-bit number for the pulse-width modulation, must be between 0 and 65535
 
 def forward():
     motora_1a.duty_cycle = speed
     motora_1b.duty_cycle = 0
-    motorb_1b.value = True
-    motorb_1a.value = False
+    motorb_1b.duty_cycle = speed
+    motorb_1a.duty_cycle = 0
     
 def stop():
     motora_1a.duty_cycle = 0
     motora_1b.duty_cycle = 0
-    motorb_1b.value = False
-    motorb_1a.value = False
-
-print("Hello World!")
+    motorb_1b.duty_cycle = 0
+    motorb_1a.duty_cycle = 0
 
 while True:
     forward()
